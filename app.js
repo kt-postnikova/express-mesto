1b628e254da8dcbed54af06dfbe72d269b2597ad
kt-postnikova <kt-postnikova@yandex.ru>
2021-07-30 12:01:57 +0300

    Создан сервер express

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const userRouter = require('./routes/user');
+const cardsRouter = require('./routes/card');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '61013c0d2454df1f8855cfaf',
+  };
+
+  next();
+});
+
+app.use('/', userRouter);
+app.use('/', cardsRouter);
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT);
