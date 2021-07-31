605f9ca05ea22b7c55cea644830a95a09e2edcbd
kt-postnikova <kt-postnikova@yandex.ru>
2021-07-31 17:38:36 +0300

    Исправлена проверка ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,11 +1,17 @@
+const mongoose = require('mongoose');
 const Card = require('../models/card');
 const { NOT_FOUND_STATUS_CODE, BAD_REQUEST_STATUS_CODE, SERVER_ERROR_STATUS_CODE } = require('../errors/errors');
 
 const getCards = (req, res) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => {
+      if (cards.length === 0) {
+        throw new mongoose.Error.DocumentNotFoundError();
+      }
+      res.send({ data: cards });
+    })
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.name === 'DocumentNotFoundError') {
         res.status(NOT_FOUND_STATUS_CODE).send({ message: 'Карточки не найдены' });
       } else {
         res.status(SERVER_ERROR_STATUS_CODE).send({ message: 'Произошла ошибка' });
@@ -20,7 +26,7 @@ const createCard = (req, res) => {
   Card.create({ name, link, owner })
     .then((cards) => res.send({ data: cards }))
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.name === 'ValidationError') {
         res.status(BAD_REQUEST_STATUS_CODE).send({ message: 'Переданы некорректные данные' });
       } else {
         res.status(SERVER_ERROR_STATUS_CODE).send({ message: 'Произошла ошибка' });
@@ -30,9 +36,14 @@ const createCard = (req, res) => {
 
 const deleteCard = (req, res) => {
   Card.findByIdAndDelete(req.params.cardId)
-    .then((card) => res.send({ data: card }))
+    .then((card) => {
+      if (!card) {
+        throw new mongoose.Error.DocumentNotFoundError();
+      }
+      res.send({ data: card });
+    })
     .catch((err) => {
-      if (err.name === 'CastError') {
+      if (err.name === 'DocumentNotFoundError') {
         res.status(BAD_REQUEST_STATUS_CODE).send({ message: 'Карточка не найдена' });
       } else {
         res.status(SERVER_ERROR_STATUS_CODE).send({ message: 'Произошла ошибка' });
@@ -43,13 +54,25 @@ const deleteCard = (req, res) => {
 const putLike = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .then((like) => res.send({ data: like }))
-    .catch(() => res.status(SERVER_ERROR_STATUS_CODE).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(NOT_FOUND_STATUS_CODE).send({ message: 'Карточка не найдена' });
+      } else {
+        res.status(SERVER_ERROR_STATUS_CODE).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
 
 const deleteLike = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then((like) => res.send({ data: like }))
-    .catch(() => res.status(SERVER_ERROR_STATUS_CODE).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(NOT_FOUND_STATUS_CODE).send({ message: 'Карточка не найдена' });
+      } else {
+        res.status(SERVER_ERROR_STATUS_CODE).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
 
 module.exports = {
