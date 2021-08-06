dd91cfe5ed21a918d547a0c1b3da4a1354ad71f4
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-06 17:17:59 +0300

    Создан контроллер login

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const { NOT_FOUND_STATUS_CODE, BAD_REQUEST_STATUS_CODE, SERVER_ERROR_STATUS_CODE } = require('../errors/errors');
@@ -41,9 +42,11 @@ const getUserById = (req, res) => {
 const createUser = (req, res) => {
   const { name, email, password, about, avatar } = req.body;
 
-  User.create({ name, email, password, about, avatar })
-    .then((users) => res.send({ data: users }))
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({ name, email, password: hash, about, avatar }))
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
+      console.log(err);
       if (err.name === 'ValidationError') {
         res.status(BAD_REQUEST_STATUS_CODE).send({ message: 'Переданы некорректные данные' });
       } else {
@@ -51,6 +54,48 @@ const createUser = (req, res) => {
       }
     });
 };
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        throw new mongoose.Error.DocumentNotFoundError();
+      }
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        throw new mongoose.Error.DocumentNotFoundError();
+      }
+      // res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        res.status(NOT_FOUND_STATUS_CODE).send({ message: 'Пользователь не зарегистрирован' });
+      }
+    });
+};
+
+// const deleteUser = (req, res) => {
+//   User.findByIdAndDelete(req.params.cardId)
+//     .then((card) => {
+//       if (!card) {
+//         throw new mongoose.Error.DocumentNotFoundError();
+//       }
+//       res.send({ data: card });
+//     })
+//     .catch((err) => {
+//       if (err.name === 'DocumentNotFoundError') {
+//         res.status(NOT_FOUND_STATUS_CODE).send({ message: 'Карточка не найдена' });
+//       } else if (err.name === 'CastError') {
+//         res.status(BAD_REQUEST_STATUS_CODE).send({ message: 'Отправлен некорректный запрос' });
+//       } else {
+//         res.status(SERVER_ERROR_STATUS_CODE).send({ message: 'Произошла ошибка' });
+//       }
+//     });
+// };
 
 const updateUserProfile = (req, res) => {
   const { name, about } = req.body;
@@ -83,6 +128,8 @@ const updateUserAvatar = (req, res) => {
 module.exports = {
   getUsers,
   getUserById,
+  // deleteUser,
+  login,
   createUser,
   updateUserProfile,
   updateUserAvatar,
