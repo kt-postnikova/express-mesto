4cfe7958afc788b7133b4fa255c8a1b5f11df244
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-14 14:16:06 +0300

    Изменены пути

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const userRouter = require('./routes/user');
 const cardsRouter = require('./routes/card');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 
@@ -11,14 +14,24 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = { _id: '61013c0d2454df1f8855cfaf' };
+// app.use((req, res, next) => {
+//   req.user = { _id: '61013c0d2454df1f8855cfaf' };
+
+//   next();
+// });
+
+app.use(cookieParser());
+app.post('/signin', login);
+app.post('/signup', createUser);
+app.use('/', auth, userRouter);
+app.use('/', auth, cardsRouter);
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
 
   next();
 });
-
-app.use('/', userRouter);
-app.use('/', cardsRouter);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
