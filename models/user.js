2e59eab922f2720ec8e60e572de2477d03de29f0
kt-postnikova <kt-postnikova@yandex.ru>
2021-08-14 14:12:22 +0300

    Добавлен валидатор для проверки email

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -15,13 +16,14 @@ const userSchema = new mongoose.Schema({
     unique: true,
     validate: {
       validator(v) {
-        validator.isEmail(v);
+        return validator.isEmail(v);
       },
     },
   },
   password: {
     type: String,
     require: true,
+    select: false,
   },
   about: {
     type: String,
@@ -32,19 +34,24 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(v) {
+        return /(http|https):\/\/(www)*[a-z0-9\-._~:/?#[\]@!$&'()*+,;=]+#*/g.test(v);
+      },
+    },
   },
 }, { versionKey: false });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
+  return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        throw new UnauthorizedError('Неправильные почта или пароль');
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            throw new UnauthorizedError('Неправильные почта или пароль');
           }
           return user;
         });
